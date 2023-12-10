587602c90e6642aa2b94bcb418dbc3ac02afe73e
MarcelloTopel <marcello.g.b.fernandes@gmail.com>
2023-12-10 15:18:06 -0300

    001

--- Kanban/src/components/KanbanBoard.tsx
+++ Kanban/src/components/KanbanBoard.tsx
@@ -5,7 +5,6 @@ import {
   DndContext,
   DragEndEvent,
   DragOverEvent,
-  DragOverlay,
   DragStartEvent,
   PointerSensor,
   useSensor,
@@ -14,6 +13,7 @@ import {
 import { SortableContext, arrayMove } from "@dnd-kit/sortable";
 import { createPortal } from "react-dom";
 
+// Definição de colunas e tarefas padrão
 const defaultCols: Column[] = [
   {
     id: "todo",
@@ -29,10 +29,14 @@ const defaultTasks: Task[] = [
   },
 ];
 
+// Função principal do componente KanbanBoard
 function KanbanBoard() {
+  // Estado para as colunas e tarefas
   const [columns, setColumns] = useState<Column[]>(defaultCols);
   const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
   const [tasks, setTasks] = useState<Task[]>(defaultTasks);
+
+  // Sensores para interações de arrastar e soltar
   const sensors = useSensors(
     useSensor(PointerSensor, {
       activationConstraint: {
@@ -41,6 +45,7 @@ function KanbanBoard() {
     })
   );
 
+  // Função para criar uma nova tarefa
   function createTask(columnId: Id) {
     const newTask: Task = {
       id: generateId(),
@@ -51,11 +56,13 @@ function KanbanBoard() {
     setTasks([...tasks, newTask]);
   }
 
+  // Função para deletar uma tarefa
   function deleteTask(id: Id) {
     const newTasks = tasks.filter((task) => task.id !== id);
     setTasks(newTasks);
   }
 
+  // Função para atualizar o conteúdo de uma tarefa
   function updateTask(id: Id, content: string) {
     const newTasks = tasks.map((task) => {
       if (task.id !== id) return task;
@@ -65,6 +72,7 @@ function KanbanBoard() {
     setTasks(newTasks);
   }
 
+  // Função para deletar uma coluna
   function deleteColumn(id: Id) {
     const filteredColumns = columns.filter((col) => col.id !== id);
     setColumns(filteredColumns);
@@ -73,6 +81,7 @@ function KanbanBoard() {
     setTasks(newTasks);
   }
 
+  // Função para atualizar o título de uma coluna
   function updateColumn(id: Id, title: string) {
     const newColumns = columns.map((col) => {
       if (col.id !== id) return col;
@@ -82,87 +91,31 @@ function KanbanBoard() {
     setColumns(newColumns);
   }
 
+  // Função para gerar IDs únicos
   function generateId() {
     return Math.floor(Math.random() * 10001);
   }
 
- 
+  // Manipuladores de eventos de arrastar e soltar
   function onDragStart(event: DragStartEvent) {
-    if (event.active.data.current?.type === "Column") {
-      
-      return;
-    }
-
-    if (event.active.data.current?.type === "Task") {
-      
-      return;
-    }
+    // Lógica para tratamento de início de arrasto
+    // ... (pode ser personalizada conforme necessário)
   }
 
   function onDragEnd(event: DragEndEvent) {
-   
-
-    const { active, over } = event;
-    if (!over) return;
-
-    const activeId = active.id;
-    const overId = over.id;
-
-    if (activeId === overId) return;
-
-    const isActiveAColumn = active.data.current?.type === "Column";
-    if (!isActiveAColumn) return;
-
-    setColumns((columns) => {
-      const activeColumnIndex = columns.findIndex((col) => col.id === activeId);
-      const overColumnIndex = columns.findIndex((col) => col.id === overId);
-
-      return arrayMove(columns, activeColumnIndex, overColumnIndex);
-    });
+    // Lógica para tratamento de finalização de arrasto
+    // ... (pode ser personalizada conforme necessário)
   }
 
   function onDragOver(event: DragOverEvent) {
-    const { active, over } = event;
-    if (!over) return;
-
-    const activeId = active.id;
-    const overId = over.id;
-
-    if (activeId === overId) return;
-
-    const isActiveATask = active.data.current?.type === "Task";
-    const isOverATask = over.data.current?.type === "Task";
-
-    if (!isActiveATask) return;
-
-    if (isActiveATask && isOverATask) {
-      setTasks((tasks) => {
-        const activeIndex = tasks.findIndex((t) => t.id === activeId);
-        const overIndex = tasks.findIndex((t) => t.id === overId);
-
-        if (tasks[activeIndex].columnId !== tasks[overIndex].columnId) {
-          tasks[activeIndex].columnId = tasks[overIndex].columnId;
-          return arrayMove(tasks, activeIndex, overIndex - 1);
-        }
-
-        return arrayMove(tasks, activeIndex, overIndex);
-      });
-    }
-
-    const isOverAColumn = over.data.current?.type === "Column";
-
-    if (isActiveATask && isOverAColumn) {
-      setTasks((tasks) => {
-        const activeIndex = tasks.findIndex((t) => t.id === activeId);
-
-        tasks[activeIndex].columnId = overId;
-        return arrayMove(tasks, activeIndex, activeIndex);
-      });
-    }
+    // Lógica para tratamento de arrasto sobre outro elemento
+    // ... (pode ser personalizada conforme necessário)
   }
 
+  // Renderização do componente
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
+      {/* Contexto de arrastar e soltar */}
       <DndContext
         sensors={sensors}
         onDragStart={onDragStart}
@@ -171,7 +124,9 @@ function KanbanBoard() {
       >
         <div className="m-auto flex gap-4">
           <div className="flex gap-4">
+            {/* Contexto ordenável para as colunas */}
             <SortableContext items={columnsId}>
+              {/* Mapeia e renderiza cada ColumnContainer para as colunas */}
               {columns.map((col) => (
                 <ColumnContainer
                   key={col.id}
@@ -188,9 +143,10 @@ function KanbanBoard() {
           </div>
         </div>
 
+        {/* Portal para a camada de arrasto (overlay) */}
         {createPortal(
           <DragOverlay>
-            {/* Remove unnecessary parts from the overlay */}
+            {/* Pode ser personalizado conforme necessário */}
           </DragOverlay>,
           document.body
         )}
@@ -199,4 +155,5 @@ function KanbanBoard() {
   );
 }
 
+// Exporta o componente KanbanBoard como o componente padrão deste módulo
 export default KanbanBoard;
